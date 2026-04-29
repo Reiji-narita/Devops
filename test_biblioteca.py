d381d4a9b68267334372cc6ac743ab4e2be0fd97
Henri <92reiji@gmail.com>
2026-04-29 18:04:16 -0300

    Update test_biblioteca.py

--- test_biblioteca.py
+++ test_biblioteca.py
@@ -12,33 +12,42 @@ class TestBiblioteca(unittest.TestCase):
 
     def test_listar_livros(self):
         self.b.adicionar_livro("Python", "Reiji")
+        self.b.adicionar_livro("Java", "Reiji")
+
         livros = self.b.listar_livros()
-        self.assertEqual(len(livros), 1)
+        self.assertEqual(len(livros), 2)
 
     def test_emprestar_livro(self):
         self.b.adicionar_livro("Python", "Reiji")
+
         resultado = self.b.emprestar_livro("Python")
         self.assertTrue(resultado)
 
     def test_emprestar_livro_ja_emprestado(self):
         self.b.adicionar_livro("Python", "Reiji")
+
         self.b.emprestar_livro("Python")
         resultado = self.b.emprestar_livro("Python")
+
         self.assertFalse(resultado)
 
     def test_devolver_livro(self):
         self.b.adicionar_livro("Python", "Reiji")
+
         self.b.emprestar_livro("Python")
         resultado = self.b.devolver_livro("Python")
+
         self.assertTrue(resultado)
 
     def test_devolver_livro_nao_emprestado(self):
         self.b.adicionar_livro("Python", "Reiji")
+
         resultado = self.b.devolver_livro("Python")
         self.assertFalse(resultado)
 
     def test_buscar_livro(self):
         self.b.adicionar_livro("Python", "Reiji")
+
         livro = self.b.buscar_livro("Python")
         self.assertIsNotNone(livro)
 
